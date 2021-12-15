ca722ac925460e9ebc17ed89f32d3ac55b9fc081
Dev829 <goldencross126@gmail.com>
2021-12-15 14:59:54 -0500

    Moved backup folder

--- views/order/order.php
+++ views/order/order.php
@@ -1,26 +1,6 @@
 <?php 
 use app\helpers\UtilityHelper;
 ?>
-<!-- <div class="header-top">
-  <div class="container">
-    <div class="nav-wrapper">
-      <div class="d-flex mr-30">
-        <p class="description white mt-15 mb-15">Call us at <span class="bold"><a class="white" href="tel:<?= UtilityHelper::getCustomParameters('SitePhone'); ?>"><?= UtilityHelper::getCustomParameters('SitePhone'); ?></a></span></p>
-      </div>
-      <div class="d-flex">
-        <img class="security" src="/products/images/order/security.png">
-      </div>
-    </div>
-  </div>
-</div>
-<div class="header-bottom">
-  <div class="container d-flex justify-content-between align-center">
-    <a href="/products/">
-      <img class="logo" src="/products/images/order/logo.png">
-    </a>
-    <button class="cta_btn">Order Now</button>
-  </div>
-</div> -->
 <div class="header-main d-flex align-center flex-direction-column section-space">
   <div class="container">
     <h1 class="text-center uppercase">ORDER SUPERIOR MALE</h1>
